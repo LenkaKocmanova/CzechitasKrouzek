2e3daea98586c16ff56b498ba51e7fb15efba3f0
LenkaKocmanova <lenka.kocmanovagmail.com>
2022-10-10 12:09:17 +0200

    lekce1

--- lekce1/main.py
+++ lekce1/main.py
@@ -3,10 +3,13 @@ hlaska = ("Už nikdy nebudu koukat na anime.")
 hlaska = (hlaska + " ") *3
 print(hlaska)
 
-# vek = input('Kolik ti je?')
-# budoucnost = input('Jaký rok tě zajímá?')
-# rozdil = int(budoucnost) - 2022
-# vek_bud = int(vek) + rozdil
-# print('V roce '+budoucnost+' ti bude '+str(vek_bud)+' let, trilobite.')
+from datetime import datetime
+rok = datetime.now().year
+
+vek = input('Kolik ti je?')
+budoucnost = input('Jaký rok tě zajímá?')
+rozdil = int(budoucnost) - rok
+vek_bud = int(vek) + rozdil
+print('V roce '+budoucnost+' ti bude '+str(vek_bud)+' let, trilobite.')
 
 
---
+++ lekce2/cviceni.py
@@ -0,0 +1,8 @@
+a = 7
+b= 10
+if (a<7):
+    print('a je mensi 7')
+elif((a>=7 and a<10)):
+    print(not(a>=7 and a<10))
+else:
+    print('Zbytek')
