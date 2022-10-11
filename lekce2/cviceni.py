0f8d0bbb0b6285fedb546db70613c4e34d8fc50d
LenkaKocmanova <lenka.kocmanovagmail.com>
2022-10-11 08:27:55 +0200

    lekce2

--- lekce2/cviceni.py
+++ lekce2/cviceni.py
@@ -1,8 +1,27 @@
-a = 7
-b= 10
-if (a<7):
-    print('a je mensi 7')
-elif((a>=7 and a<10)):
-    print(not(a>=7 and a<10))
+#
+# Je přítomen štípač? ano/ne: ano
+# Je přítomen utěrač potu? ano/ne: ano
+# Je přítomen instruktor? ano/ne: ano
+# Příklad výstupu:
+# No a je to!
+
+dotaz = input('Je přítomen štípač? ano/ne:')
+if (dotaz == 'ano'):
+    stipac = True
 else:
-    print('Zbytek')
+    stipac = False
+dotaz = input('Je přítomen utěrač potu? ano/ne:')
+if (dotaz == 'ano'):
+    uterac = True
+else:
+    uterac = False
+dotaz = input('Je přítomen instruktor? ano/ne:')
+if (dotaz == 'ano'):
+    instruktor = True
+else:
+    instruktor = False
+
+if((stipac and uterac and instruktor) or (not stipac and not uterac and instruktor)):
+    print('No a je to!')
+else:
+    print('Aaaa bum!')
