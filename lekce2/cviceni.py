828453934cfdfc56ef2a902b76cdf762cffecc2a
LenkaKocmanova <lenka.kocmanovagmail.com>
2022-10-11 09:16:30 +0200

    lekce2

--- lekce2/cviceni.py
+++ lekce2/cviceni.py
@@ -5,22 +5,9 @@
 # Příklad výstupu:
 # No a je to!
 
-dotaz = input('Je přítomen štípač? ano/ne:')
-if (dotaz == 'ano'):
-    stipac = True
-else:
-    stipac = False
-dotaz = input('Je přítomen utěrač potu? ano/ne:')
-if (dotaz == 'ano'):
-    uterac = True
-else:
-    uterac = False
-dotaz = input('Je přítomen instruktor? ano/ne:')
-if (dotaz == 'ano'):
-    instruktor = True
-else:
-    instruktor = False
-
+stipac = input('Je přítomen štípač? ano/ne:') == 'ano'
+uterac = input('Je přítomen utěrač potu? ano/ne:') == 'ano'
+instruktor = input('Je přítomen instruktor? ano/ne:') == 'ano'
 if((stipac and uterac and instruktor) or (not uterac and instruktor)):
     print('No a je to!')
 else:
